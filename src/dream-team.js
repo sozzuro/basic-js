add7f9fe79cc45fc4aa97382f4e3b24e56455445
Denis Radivil <sozzuro@gmail.com>
2020-09-23 20:08:26 +0300

    feat: complete task what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,16 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let arr = []
+    let arr = []
 
-  if (!Array.isArray(members)) {
-    return false
-  }
-  members.forEach(element => {
-    if (typeof element === 'string') {
-      arr.push(element.trim()[0].toLocaleUpperCase())
+    if (!Array.isArray(members)) {
+      return false
     }
-  });
+    members.forEach(element => {
+      if (typeof element === 'string') {
+        arr.push(element.trim()[0].toLocaleUpperCase())
+      }
+    });
 
-  return arr.sort().join('');
+    return arr.sort().join('');
 };
