bc19746295a9d48cdaf530000832e0e0dbcb955a
Denis Radivil <sozzuro@gmail.com>
2020-09-23 19:40:54 +0300

    feat: complete task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = []
+
+  if (!Array.isArray(members)) {
+    return false
+  }
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      arr.push(element.trim()[0].toLocaleUpperCase())
+    }
+  });
+
+  return arr.sort().join('');
 };
