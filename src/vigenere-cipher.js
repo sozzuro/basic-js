ca991d28eae28913f3bdf77454b7f52f6c382979
Denis Radivil <sozzuro@gmail.com>
2020-11-01 13:20:07 +0300

    feat: complete task vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,80 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(Reverse = true) {
+      this.isReverse = Reverse;
+  }
+
+  encrypt(message, key) {
+      if (message === undefined || key === undefined) {
+          throw Error
+      }
+
+      let messageArray = message.toUpperCase().split('');
+      let keyArray = key.toUpperCase().split('');
+      let encryptLetterOfMessage = '';
+      let encryptArray = [];
+
+      for (var i = 0, j = 0; i < messageArray.length; i++, j++) {
+          if (j >= keyArray.length) {
+              j = 0;
+          }
+
+          encryptLetterOfMessage = messageArray[i].codePointAt();
+
+          if (encryptLetterOfMessage >= 65 && encryptLetterOfMessage <= 90) {
+              encryptLetterOfMessage += (keyArray[j].codePointAt() - 65);
+              if (encryptLetterOfMessage > 90) {
+                  encryptLetterOfMessage -= 26;
+              }
+          } else {
+              j--;
+          }
+
+          encryptArray.push(String.fromCodePoint(encryptLetterOfMessage));
+      }
+
+      if (this.isReverse === false) {
+          encryptArray.reverse();
+      }
+
+      return encryptArray.join('');
+  }
+
+  decrypt(message, key) {
+      if (message === undefined || key === undefined) {
+          throw Error
+      }
+
+      let messageArray = message.toUpperCase().split('');
+      let keyArray = key.toUpperCase().split('');
+      let encryptLetterOfMessage = '';
+      let encryptArray = [];
+
+      for (let i = 0, j = 0; i < messageArray.length; i++, j++) {
+          if (j >= keyArray.length) {
+              j = 0;
+          }
+
+          encryptLetterOfMessage = messageArray[i].codePointAt();
+
+          if (encryptLetterOfMessage >= 65 && encryptLetterOfMessage <= 90) {
+              encryptLetterOfMessage -= (keyArray[j].codePointAt() - 65);
+              if (encryptLetterOfMessage < 65) {
+                  encryptLetterOfMessage += 26;
+              }
+          } else {
+              j--;
+          }
+
+          encryptArray.push(String.fromCodePoint(encryptLetterOfMessage));
+      }
+
+      if (this.isReverse === false) {
+          encryptArray.reverse();
+      }
+
+      return encryptArray.join('');
   }
 }
 
