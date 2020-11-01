bf484f5685e397ecfa252513ad1b78ff3ab3e91d
Denis Radivil <sozzuro@gmail.com>
2020-11-01 12:26:34 +0300

    feat: complete task axtended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatString = String(str);
+  let resultString = '';
+
+  if (options.repeatTimes !== undefined) {
+      for (let i = 0; i < options.repeatTimes; i++) {
+          resultString += repeatString;
+
+          if (options.additionRepeatTimes !== undefined) {
+              for (let j = 0; j < options.additionRepeatTimes; j++) {
+                  resultString += options.addition;
+                  if (j !== options.additionRepeatTimes - 1) {
+                      (options.additionSeparator === undefined) ? resultString += '|': resultString += options.additionSeparator;;
+                  }
+              }
+          }
+
+          if (i !== options.repeatTimes - 1) {
+              (options.separator === undefined) ? resultString += '+': resultString += options.separator;
+          }
+      }
+      return resultString;
+  }
+  return repeatString + options.addition;
 };
   
