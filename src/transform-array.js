b50049064da858468b332d3fbea6e70c707b7cbe
Denis Radivil <sozzuro@gmail.com>
2020-11-01 10:41:49 +0300

    fix: change checking conditions in transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,33 +7,17 @@ module.exports = function transform(arr) {
 
   let newArr = []
 
-  for (let index = 0; index < arr.length; index++) {
-
-    if (arr[index] === '--discard-next') {
-      index++;
-    } else if (arr[index] === '--discard-prev') {
-      if (index > 0) {
-        newArr.pop();
-      } else {
-        continue;
-      }
-
-    } else if (arr[index] === '--double-next') {
-      if (arr.length > index + 1) {
-        newArr.push(arr[index + 1]);
-      } else {
-        continue
-      }
-
-    } else if (arr[index] === '--double-prev') {
-      if (index > 0) {
-        newArr.push(arr[index - 1]);
-      } else {
-        continue;
-      }
-
-    } else {
-      newArr.push(arr[index]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') {
+      newArr.push(arr[i]);
+    } else if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      i++;
+    } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next' && i !== 0) {
+      newArr.pop();
+    } else if (arr[i] === '--double-next' && i !== arr.length - 1) {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next' && i !== 0) {
+      newArr.push(arr[i - 1]);
     }
   }
 
